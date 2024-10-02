5dc2f1d11eb0453420df033328c444825b122657
Yakovleva <v.yakovleva@inostudio.com>
2024-10-02 23:53:07 +0300

    init

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,5 @@
 const copy = async () => {
-    // Write your code here 
+    /* creates new file fresh.txt with content I am fresh and young inside of the files folder (if file already exists Error with message FS operation failed must be thrown)*/
 };
 
 await copy();
