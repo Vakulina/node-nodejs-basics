0eb2b620a2061f85fb425cfc42f9933b6ec47f4f
Yakovleva <v.yakovleva@inostudio.com>
2024-10-06 23:49:22 +0300

    hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs/promises'; // Импортируем readFile из 'fs/promises'
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const dirName = dirname(fileURLToPath(import.meta.url));
+const sourceDir = join(dirName, 'files');
+const filePath = join(sourceDir, 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const content = await readFile(filePath, { encoding: 'utf8' });
+
+    const hash = createHash('sha256');
+    console.log(hash.update(content).digest('hex'));
 };
 
 await calculateHash();
