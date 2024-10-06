d95ea87c0205f728a80bb6b3f3cd655051b225aa
Yakovleva <v.yakovleva@inostudio.com>
2024-10-06 21:02:44 +0300

    list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,7 @@
+import * as fs from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 const dirName = dirname(fileURLToPath(import.meta.url));
 const filesDir = join(dirName, 'files');
 
@@ -11,7 +15,15 @@ const checkFileExists = async (filePath) => {
 }
 
 const list = async () => {
-    // Write your code here 
+    const isFilesExist = await checkFileExists(filesDir)
+    if (!isFilesExist) {
+        throw new Error('FS operation failed')
+    }
+    const list = []
+    const files = await fs.readdir(filesDir);
+    for (const file of files) list.push(file);
+    console.log(list)
+
 };
 
 await list();
