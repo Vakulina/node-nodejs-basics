6e7ef4f2df00182860bae31012c8ff51642c57e7
Yakovleva <v.yakovleva@inostudio.com>
2024-10-06 20:52:06 +0300

    rename

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,15 @@
+const dirName = dirname(fileURLToPath(import.meta.url));
+const filesDir = join(dirName, 'files');
+
+const checkFileExists = async (filePath) => {
+    try {
+        await fs.access(filePath);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
 const list = async () => {
     // Write your code here 
 };
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import * as fs from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const dirName = dirname(fileURLToPath(import.meta.url));
+const oldPath = join(dirName, 'files', 'wrongFilename.txt');
+const newPath = join(dirName, 'files', 'properFilename.md')
+console.log(oldPath)
+const checkFileExists = async (filePath) => {
+    try {
+        await fs.access(filePath);
+        return true;
+    } catch {
+        return false;
+    }
+}
 const rename = async () => {
-    // Write your code here 
+    const isFileExist = await checkFileExists(oldPath);
+    if (!isFileExist) {
+        throw new Error('FS operation failed');
+    } else {
+        fs.rename(oldPath, newPath);
+    }
+
 };
 
 await rename();
