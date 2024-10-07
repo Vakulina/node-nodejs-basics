feabf60d3582ae613b9d8ec10ea9054bcacb751c
Victoria Yakovleva <v.yakovleva@inostudio.net>
2024-10-07 11:41:40 +0300

    feat/readStream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import * as fs from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const dirName = dirname(fileURLToPath(import.meta.url));
+const filesDir = join(dirName, "files");
+
 const read = async () => {
-    // Write your code here 
+  const stream = fs.createReadStream(join(filesDir, "fileToRead.txt"));
+  stream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  stream.on("end", () => {
+    console.log("\n");
+  });
+
 };
 
 await read();
