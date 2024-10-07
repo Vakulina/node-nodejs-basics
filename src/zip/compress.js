f389ed07d4b3e0e8edba9a23d8cee6b382970a52
Victoria Yakovleva <v.yakovleva@inostudio.net>
2024-10-07 12:28:33 +0300

    feat/zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
-const compress = async () => {
-    // Write your code here 
+import { pipeline } from 'node:stream/promises';
+import * as fs from 'node:fs';
+import * as zlib from 'node:zlib';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const dirName = dirname(fileURLToPath(import.meta.url));
+const filePath = join(dirName, "files", "fileToCompress.txt");
+
+const compress =    async () => {
+    await pipeline(
+        fs.createReadStream(filePath),
+        zlib.createGzip(),
+        fs.createWriteStream(join(dirName, "files",'archive.gz')),
+      );
+      console.log('Pipeline succeeded.');
 };
 
 await compress();
