028fbba9145d0d690ef71840222576dc434a2099
Victoria Yakovleva <v.yakovleva@inostudio.net>
2024-10-07 12:36:49 +0300

    feat/zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import { dirname, join } from 'path';
+import {  fileURLToPath } from 'url';
+
+const dirName = dirname(fileURLToPath(import.meta.url));
+const archivePath = join(dirName, 'files', 'archive.gz');
+const outputPath = join(dirName, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+  const gunzip = zlib.createGunzip(); 
+
+  const writeStream = fs.createWriteStream(outputPath);
+
+  fs.createReadStream(archivePath) 
+    .pipe(gunzip)                  
+    .pipe(writeStream)            
+    .on('finish', () => {
+      console.log('Распаковка завершена.');
+    })
+    .on('error', (err) => {
+      console.error('Ошибка при распаковке:', err);
+    });
 };
+
 
 await decompress();
