060702e5a689529f30bcc4be24a60ac633f14333
Victoria Yakovleva <v.yakovleva@inostudio.net>
2024-10-07 13:34:35 +0300

    feat/worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,42 @@
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const os = await import("node:os");
+const { Worker } = await import("node:worker_threads");
+
+const dirName = dirname(fileURLToPath(import.meta.url));
+const workerFilePath = join(dirName, 'worker.js');
+
+const cpuCount = os.cpus().length; 
+
 const performCalculations = async () => {
-    // Write your code here
+  const runWorker = (workerData) => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(workerFilePath, { workerData });
+
+      worker.on("message", resolve);
+      worker.on("error", reject);
+      worker.on("exit", (code) => {
+        if (code !== 0) {
+          reject(new Error(`Worker stopped with exit code ${code}`));
+        }
+      });
+    });
+  };
+
+  const promises = [];
+  let startNumber = 10; 
+
+  for (let i = 0; i < cpuCount; i++, startNumber++) {
+    promises.push(runWorker(startNumber).catch((error) => {
+      console.error(`Error in worker for ${startNumber}:`, error);
+      return { status: 'error', data: null };
+    }));
+  }
+
+  const results = await Promise.all(promises);
+  console.log('Results:', results); 
 };
+
 
 await performCalculations();
